7e497f677627784f465dbbf6ddafe79afab0cccb
Kanchan-Baran-Hazra <kanchanbhazra7063@gmail.com>
2025-08-08 01:41:28 +0530

    Updated project files before redeploy

--- app.py
+++ app.py
@@ -1001,5 +1001,5 @@ def sucess_sub():
 if __name__=='__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
